e8b84b69eeb49d070a8d2c1e4fd1916c515c583c
Louis Schnellbach <louis.schnellbach@gmail.com>
2015-11-04 08:18:23 +0100

    Load MetaDB, Log and VM in UnitTests

--- unittests/main.cpp
+++ unittests/main.cpp
@@ -1,5 +1,16 @@
 
+#include <Muon/System/Log.hpp>
+#include "Haize/VM.hpp"
+
 int main(int argc, char** argv)
 {
+	if(!muon::meta::MetaDatabase::isInstantiated())
+	{
+		muon::meta::MetaDatabase::instantiate();
+	}
+	muon::system::Log::registerDefaultLogImpl();
+	hz::VMInstance vm;
+
+	muon::system::Log::close();
 	return 0;
 }
