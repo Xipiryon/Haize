04fb63a3f3711aba12bafe5eda889eb88e3e0032
Xipiryon <louis.schnellbach@gmail.com>
2016-03-15 10:33:31 +0000

    Updated Premake to generate lemon files

--- project_Lib.lua
+++ project_Lib.lua
@@ -1,6 +1,16 @@
 
 -- Library
 -------------------------------------------
+function lemon(ProjectRoot)
+	local lemonPath = ProjectRoot.."/src/Haize/Parser/lemon/"
+	local lemonFile = "lemon.haize"
+	local lemonGen = "lemon.c"
+	os.chdir(lemonPath)
+	os.execute("rm "..lemonGen.."pp") -- remove .cpp
+	os.execute("lemon -s "..lemonFile)
+	os.execute("mv "..lemonGen.." "..lemonGen.."pp") -- rename .c to .cpp
+	os.chdir(ProjectRoot)
+end
 
 project "HaizeLibrary"
 	local ProjectRoot = os.getcwd()
@@ -21,3 +31,11 @@ project "HaizeLibrary"
 	filter "*DLL"
 		defines { "HAIZE_EXPORTS" }
 
+newaction {
+	trigger	 = "lemon",
+	description = "Generate Lemon files",
+
+	execute = function ()
+		lemon(ProjectRoot)
+	end
+}
