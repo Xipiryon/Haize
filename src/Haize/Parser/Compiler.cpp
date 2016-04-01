e55af70d56248f91a0fbc7197429f6bdc392ba06
Xipiryon <louis.schnellbach@gmail.com>
2016-04-01 19:11:37 +0200

    Removed Load file

--- src/Haize/Parser/Compiler.cpp
+++ src/Haize/Parser/Compiler.cpp
@@ -1,3 +1,4 @@
+#include <fstream>
 #include <sstream>
 #include <Muon/System/Log.hpp>
 
@@ -107,6 +108,7 @@ namespace ascii
 
 	void display(hz::parser::ASTNode* node)
 	{
+#if defined(HAIZE_DEBUG)
 		printf(" %s (%s)\n", node->name.cStr(), utils::tokenValueToStr(node).cStr());
 		for (m::u32 i = 0; i < node->children->size(); ++i)
 		{
@@ -131,6 +133,7 @@ namespace ascii
 			}
 			display(n);
 			pop();
+#endif
 		}
 	}
 }
@@ -154,6 +157,45 @@ namespace hz
 		Compiler::~Compiler()
 		{
 			MUON_DELETE(m_nodeRoot);
+		}
+
+		bool Compiler::load(const char* buffer, Error& error)
+		{
+			error.clear();
+			m_loadBuffer += buffer;
+			error.state = Error::SUCCESS;
+			return true;
+		}
+
+		bool Compiler::load(const m::String& file, Error& error)
+		{
+			error.clear();
+			error.state = Error::ERROR;
+			std::ifstream stream(file.cStr());
+			if (!stream)
+			{
+				error.message = "Couldn't open the stream!";
+				return false;
+			}
+
+			if (!stream.eof())
+			{
+				// Get stream size
+				stream.seekg(0, stream.end);
+				m::u64 length = (m::u64)stream.tellg();
+				stream.seekg(0, stream.beg);
+
+				char* buffer = (char*)malloc((m::u32)length + 1);
+				stream.read(buffer, length);
+				buffer[stream.gcount()] = 0;
+				m_loadBuffer += buffer;
+				free(buffer);
+
+				error.state = Error::SUCCESS;
+				return true;
+			}
+			error.message = "File stream seems to be empty: encountered EOF at beginning!";
+			return false;
 		}
 
 		bool Compiler::compile(Error& error)
