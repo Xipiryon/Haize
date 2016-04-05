304e9494584a5b7222e30c2036840c277919967e
Xipiryon <louis.schnellbach@gmail.com>
2016-04-05 16:03:36 +0200

    Better Precedence / Associativity Table

--- src/Haize/Parser/Syntaxic.cpp
+++ src/Haize/Parser/Syntaxic.cpp
@@ -53,19 +53,6 @@ namespace
 		}
 	}
 
-	enum OpAssociativity
-	{
-		ASSOC_LEFT,
-		ASSOC_RIGHT,
-	};
-
-	struct OpAttribute
-	{
-		m::u32 precedence;
-		OpAssociativity associativity;
-	};
-	static const OpAttribute s_OpDefault = { 0, ASSOC_LEFT };
-
 	/*
 	* Custom SharedData
 	* Specific for Lexical parsing
@@ -96,80 +83,91 @@ namespace
 		bool funcCstrDstr;
 	};
 
-	static const OpAttribute precAttrib[hz::parser::E_TERMINALTOKEN_END] =
+	enum OpAssociativity
 	{
-		s_OpDefault, //S_INVALID,
-		s_OpDefault, //S_EOF,
-
-		s_OpDefault, //S_COMMA,			','
-		{ 85, ASSOC_RIGHT },	//S_ACCESSOR,		'.'
-		s_OpDefault, //S_SEPARATOR,		New line, or ';'
-		s_OpDefault, //S_KEYWORD,
-		s_OpDefault, //S_LPARENT,		'('
-		s_OpDefault, //S_RPARENT,		')'
-		s_OpDefault, //S_LBRACKET,		'['
-		s_OpDefault, //S_RBRACKET,		']'
-		s_OpDefault, //S_LBRACE,		'{'
-		s_OpDefault, //S_RBRACE,		'}'
-
-		s_OpDefault, //E_VALUE_BEGIN,
-		s_OpDefault, //V_NUMBER,			A number (in either dec, oct, bin or hex format)
-		s_OpDefault, //V_IDENTIFIER,		An alphanumeric identifier
-		s_OpDefault, //V_STRING,			A string contained between two '"' character
-		s_OpDefault, //V_NIL,
-		s_OpDefault, //V_TRUE,
-		s_OpDefault, //V_FALSE,
-		s_OpDefault, //E_VALUE_END,
-
-		s_OpDefault, //E_LOGIC_OP_BEGIN,
-		{ 55, ASSOC_LEFT },		//LOGIC_EQ,			'=='
-		{ 55, ASSOC_LEFT },		//LOGIC_NEQ,		'!=' or 'not' keyword
-		{ 60, ASSOC_LEFT },		//LOGIC_LT,			'<'
-		{ 60, ASSOC_LEFT },		//LOGIC_LET,		'<='
-		{ 60, ASSOC_LEFT },		//LOGIC_GT,			'>'
-		{ 60, ASSOC_LEFT },		//LOGIC_GET,		'>='
-		{ 80, ASSOC_RIGHT },	//LOGIC_NOT,		'!'
-		{ 30, ASSOC_LEFT },		//LOGIC_AND,		'&&' or 'and' keyword
-		{ 25, ASSOC_LEFT },		//LOGIC_OR,			'||' or 'or' keyword
-		s_OpDefault, //E_LOGIC_OP_END,
-
-		s_OpDefault, //E_BITWISE_OP_BEGIN,
-		{ 50, ASSOC_LEFT },		//BITWISE_AND,		'&'
-		{ 35, ASSOC_LEFT },		//BITWISE_OR,		'|'
-		{ 40, ASSOC_LEFT },		//BITWISE_XOR,		'^'
-		{ 80, ASSOC_RIGHT },	//BITWISE_NOT,		'~'
-		{ 65, ASSOC_LEFT },		//BITWISE_LSH,		'<<'
-		{ 65, ASSOC_LEFT },		//BITWISE_RSH,		'>>'
-		s_OpDefault, //E_BITWISE_OP_END,
-
-		{ 80, ASSOC_RIGHT },	//UNARY_PLUS,		'+' before an expression (is optional: +5 <=> 5)
-		{ 80, ASSOC_RIGHT },	//UNARY_MINUS,		'-' before an expression (-5 is not parsed as 0-5)
-
-		s_OpDefault, //E_MATH_OP_BEGIN,
-		{ 70, ASSOC_LEFT },		//MATH_ADD,			'+'
-		{ 70, ASSOC_LEFT },		//MATH_SUB,			'-'
-		{ 75, ASSOC_LEFT },		//MATH_MUL,			'*'
-		{ 75, ASSOC_LEFT },		//MATH_DIV,			'/'
-		{ 75, ASSOC_LEFT },		//MATH_MOD,			'%'
-		{ 80, ASSOC_RIGHT },	//MATH_INC,			'++'
-		{ 80, ASSOC_RIGHT },	//MATH_DEC,			'--'
-		s_OpDefault, //E_MATH_OP_END,
-
-		s_OpDefault, //E_ASN_OP_BEGIN,
-
-		{ 10, ASSOC_RIGHT },	//MATH_ASN,			'='
-		{ 10, ASSOC_RIGHT },	//MATH_ASN_ADD,		'+='
-		{ 10, ASSOC_RIGHT },	//MATH_ASN_SUB,		'-='
-		{ 10, ASSOC_RIGHT },	//MATH_ASN_MUL,		'/='
-		{ 10, ASSOC_RIGHT },	//MATH_ASN_DIV,		'*='
-		{ 10, ASSOC_RIGHT },	//MATH_ASN_MOD,		'%='
-		{ 10, ASSOC_RIGHT },	//MATH_ASN_AND,		'&='
-		{ 10, ASSOC_RIGHT },	//MATH_ASN_OR,		'|='
-		{ 10, ASSOC_RIGHT },	//MATH_ASN_XOR,		'^='
-		{ 10, ASSOC_RIGHT },	//MATH_ASN_NOT,		'~='
-
-		s_OpDefault, //E_ASN_OP_END,
+		ASSOC_LEFT,
+		ASSOC_RIGHT,
 	};
+
+	struct OpInfo
+	{
+		OpInfo()
+			: precedence(0)
+			, associativity(ASSOC_LEFT)
+		{
+		}
+
+		OpInfo(m::u32 prec, OpAssociativity assoc)
+			: precedence(prec)
+			, associativity(assoc)
+		{
+		}
+
+		m::u32 precedence;
+		OpAssociativity associativity;
+	};
+
+	struct OpAttributeTable
+	{
+		OpAttributeTable()
+		{
+			table[hz::parser::MATH_ASN] = OpInfo(10, ASSOC_RIGHT);		//'='
+			table[hz::parser::MATH_ASN_ADD] = OpInfo(10, ASSOC_RIGHT);	//'+='
+			table[hz::parser::MATH_ASN_SUB] = OpInfo(10, ASSOC_RIGHT);	//'-='
+			table[hz::parser::MATH_ASN_MUL] = OpInfo(10, ASSOC_RIGHT);	//'/='
+			table[hz::parser::MATH_ASN_DIV] = OpInfo(10, ASSOC_RIGHT);	//'*='
+			table[hz::parser::MATH_ASN_MOD] = OpInfo(10, ASSOC_RIGHT);	//'%='
+			table[hz::parser::MATH_ASN_AND] = OpInfo(10, ASSOC_RIGHT);	//'&='
+			table[hz::parser::MATH_ASN_OR] = OpInfo(10, ASSOC_RIGHT);	//'|='
+			table[hz::parser::MATH_ASN_XOR] = OpInfo(10, ASSOC_RIGHT);	//'^='
+			table[hz::parser::MATH_ASN_NOT] = OpInfo(10, ASSOC_RIGHT);	//'~='
+
+			table[hz::parser::LOGIC_OR] = OpInfo(25, ASSOC_LEFT);		//'||'
+
+			table[hz::parser::LOGIC_AND] = OpInfo(30, ASSOC_LEFT);		//'&&'
+
+			table[hz::parser::BITWISE_OR] = OpInfo(35, ASSOC_LEFT);		//'|'
+
+			table[hz::parser::BITWISE_XOR] = OpInfo(40, ASSOC_LEFT);	//'^'
+
+			table[hz::parser::BITWISE_AND] = OpInfo(50, ASSOC_LEFT);	//'&'
+
+			table[hz::parser::LOGIC_EQ] = OpInfo(55, ASSOC_LEFT);		//'=='
+			table[hz::parser::LOGIC_NEQ] = OpInfo(55, ASSOC_LEFT);		//'!='
+
+			table[hz::parser::LOGIC_LT] = OpInfo(60, ASSOC_LEFT);		//'<'
+			table[hz::parser::LOGIC_LET] = OpInfo(60, ASSOC_LEFT);		//'<='
+			table[hz::parser::LOGIC_GT] = OpInfo(60, ASSOC_LEFT);		//'>'
+			table[hz::parser::LOGIC_GET] = OpInfo(60, ASSOC_LEFT);		//'>='
+
+			table[hz::parser::BITWISE_LSH] = OpInfo(65, ASSOC_LEFT);	//'<<'
+			table[hz::parser::BITWISE_RSH] = OpInfo(65, ASSOC_LEFT);	//'>>'
+
+			table[hz::parser::MATH_ADD] = OpInfo(70, ASSOC_LEFT);		//'+'
+			table[hz::parser::MATH_SUB] = OpInfo(70, ASSOC_LEFT);		//'-'
+
+			table[hz::parser::MATH_MUL] = OpInfo(75, ASSOC_LEFT);		//'*'
+			table[hz::parser::MATH_DIV] = OpInfo(75, ASSOC_LEFT);		//'/'
+			table[hz::parser::MATH_MOD] = OpInfo(75, ASSOC_LEFT);		//'%'
+
+			table[hz::parser::UNARY_PLUS] = OpInfo(80, ASSOC_RIGHT);	//'+' before an expression (is optional: +5 <=> 5)
+			table[hz::parser::UNARY_MINUS] = OpInfo(80, ASSOC_RIGHT);	//'-' before an expression (-5 is not parsed as 0-5)
+			table[hz::parser::BITWISE_NOT] = OpInfo(80, ASSOC_RIGHT);	//'~'
+			table[hz::parser::LOGIC_NOT] = OpInfo(80, ASSOC_RIGHT);		//'!'
+			table[hz::parser::MATH_INC] = OpInfo(80, ASSOC_RIGHT);		//'++'
+			table[hz::parser::MATH_DEC] = OpInfo(80, ASSOC_RIGHT);		//'--'
+
+			table[hz::parser::S_ACCESSOR] = OpInfo(85, ASSOC_RIGHT);	//'.'
+		}
+
+		OpInfo operator[](hz::parser::eTokenType type) const
+		{
+			return table[type];
+		}
+
+		OpInfo table[hz::parser::E_TERMINALTOKEN_END];
+	};
+	const OpAttributeTable s_PrecedenceTable;
 
 	bool readToken(InternalSyntaxicData* impl, hz::parser::Token& out, m::u32 index, bool fillError)
 	{
